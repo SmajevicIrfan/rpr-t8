9972d5a3ac0d6065735ea1d5d17ead8e0f917530
smajevicirfan <smajevic.irfan@gmail.com>
2018-12-13 10:25:55 +0100

    Created desgin for mail sender

---
+++ src/ba/unsa/etf/rpr/tutorijal08/MailSenderController.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.tutorijal08;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MailSenderController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
